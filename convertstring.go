ebf39a1d2aefa377e35d49b7b4d84e6399b7ffab
leosha69 <leosha0303@gmail.com>
2022-07-08 20:20:05 +0300

    Create project

---
+++ convertstring.go
@@ -0,0 +1,15 @@
+package convertstring
+
+import "strconv"
+
+func StrToInt(s string) (int) {
+    num, _ := strconv.Atoi(s)
+    
+    return num
+}
+
+func StrToFloat(s string) (float64) {
+    num, _ := strconv.ParseFloat(s, 64)
+    
+    return num
+}
